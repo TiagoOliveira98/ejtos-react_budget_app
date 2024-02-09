5f733ed9028a9410152daa12f6c380a8a1cd1979
TiagoOliveira98 <tiago.mmadalenooliveira@gmail.com>
2024-02-09 17:25:36 +0000

    Final Update

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,31 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { dispatch, remaining ,currency} = useContext(AppContext);
+    const { budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const upperLimitValue = 20000
+
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+    }
+
+    const handleKeyDown = (e) => {
+        if ( e.key === 'Enter' && newBudget <= upperLimitValue && remaining > 0) {
+          dispatch({ type: "SET_BUDGET", payload: newBudget });
+        }else if (newBudget > upperLimitValue) {
+            alert(`The value cannot exceed remaining funds  ${currency}` + remaining);   
+        }else if ( remaining <= 0) {
+            alert("You cannot reduce the budget value lower than the spending");   
+        }
+    };
+
+    return (
+<div className='alert alert-secondary'>
+<span>Budget {currency}</span>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange} onKeyDown={handleKeyDown}></input>
+</div>
+    );
+};
+export default Budget;
